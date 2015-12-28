b7c194f7a6490dcfce0ab480e721489e26e8363f
Kenneth Kataiwa <kenkataiwa@outlook.com>
2015-12-28 18:35:04 +0300

    Improve navigation

--- app/src/main/java/com/kenkataiwa/kamusi/AboutActivity.java
+++ app/src/main/java/com/kenkataiwa/kamusi/AboutActivity.java
@@ -1,11 +1,11 @@
 package com.kenkataiwa.kamusi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -15,6 +15,23 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 
